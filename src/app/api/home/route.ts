bb9b26f8911c3276f980d7d4083f6802605d75e8
Cooper Gamble <coopergamble@Coopers-MacBook-Pro.local>
2024-05-24 13:22:04 -0700

    minor: log token count

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: Request) {
     const supabaseURL = process.env.SUPABASE_URL || '';
     const supabaseAnonKey = process.env.SUPABASE_SECRET_KEY || '';
     const supabase = createClient(supabaseURL, supabaseAnonKey);
-    
+
     const { data, error } = await supabase.from('tokens').select('*');
     if (error || !data) {
         console.log(error, data);
@@ -14,7 +14,7 @@ export async function GET(request: Request) {
             { status: 404 }
         );
     }
-    
+    console.log(`Found ${data.length} tokens`)
     return new Response(
         JSON.stringify(data),
         { status: 200 }
